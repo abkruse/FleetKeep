32ad8bfbeeea3b402cdd5447c90be58dc92b3b60
Alex Southwell <abkruse@gmail.com>
2016-06-29 11:00:05 -0700

    Some basic styling and factory and controller basics made

---
+++ client/app/js/report-feature/report.controller.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('fleetkeep')
+    .controller('ReportCtrl', ReportCtrl);
+
+    ReportCtrl.$inject= ['ReportFactory'];
+
+    function ReportCtrl(ReportFactory) {
+
+
+    }
+
+})();
---
+++ client/app/js/report-feature/report.factory.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('fleetkeep')
+    .factory('ReportFactory', ReportFactory);
+
+    ReportFactory.$inject = [];
+
+    function ReportFactory() {
+      return {
+
+      }
+    }
+
+})();
