538777d9ab9ac4e9393636a3796d7d7108247428
Alex Southwell <abkruse@gmail.com>
2016-06-29 17:52:09 -0700

    Create reports routes and they are working

--- server/routes/report.js
+++ server/routes/report.js
@@ -1,10 +1,48 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
+var Reports = require('../models/reports');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Reports.getAll().then( (data) => {
+    console.log(data);
+    res.send(data);
+  });
 });
+
+router.get('/:id', function(req, res, next) {
+  Reports.getOne(req.params.id).then( (data)=> {
+    console.log(data);
+    res.send(data);
+  });
+});
+
+router.get('/:id/driver', function(req, res, next) {
+  Reports.getByDriver(req.params.id).then( (data)=> {
+    console.log(data);
+    res.send(data);
+  });
+});
+
+router.post('/', function(req, res, next) {
+  Reports.add(req.body).then( (data)=> {
+    console.log(data);
+    res.send(data);
+  });
+});
+
+router.put('/:id/update', function(req, res, next) {
+  Reports.update(req.params.id, req.body).then( (data) => {
+    console.log(data);
+    res.sendStatus(200);
+  });
+});
+
+router.delete('/:id/delete', function(req, res, next) {
+  Reports.remove(req.params.id).then( (data) => {
+    console.log(data);
+    res.sendStatus(200);
+  })
+})
 
 module.exports = router;
