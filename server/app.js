a0ee0b48093f908dc226808e81fbbe282a0f478d
Alex Southwell <abkruse@gmail.com>
2016-07-05 16:08:01 -0700

    Changes to prepare for heroku deploy

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client')));
 app.options('*', cors());
 app.use(cors());
 
